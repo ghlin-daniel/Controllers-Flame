f1e7a87b4595919c04ea05c7bab8bc22c1980584
Daniel Lin <ghlin.daniel@gmail.com>
2023-02-25 16:06:17 -0800

    Added example

---
+++ example/android/app/src/main/kotlin/com/example/controllers_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.controllers_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GameWidget(game: MyGame()));
+}
+
+class MyGame extends FlameGame {
+  @override
+  Color backgroundColor() => Colors.lightBlue;
+}
