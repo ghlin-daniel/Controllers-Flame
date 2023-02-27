3b413a362e4f480b86c5c5446b6bd827332917e3
Daniel Lin <ghlin.daniel@gmail.com>
2023-02-26 23:30:55 -0800

    Added cross controller and example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,68 @@
+import 'dart:math';
+
+import 'package:controllers_flame/controllers_flame.dart';
+import 'package:flame/components.dart';
+import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(GameWidget(game: MyGame()));
+class Tank extends SpriteComponent {
+  static final angles = {
+    Vector2(1, 0): 0.0,
+    Vector2(0, -1): -pi / 2,
+    Vector2(-1, 0): pi,
+    Vector2(0, 1): pi / 2
+  };
+
+  final _speed = 3;
+
+  @override
+  Future<void> onLoad() async {
+    sprite = await Sprite.load('tank.png');
+    size = Vector2(50, 50);
+    anchor = Anchor.center;
+  }
+
+  move(Vector2 direction) {
+    final x = direction.x;
+    final y = direction.y;
+
+    if (x == 0 && y == 0) {
+      return;
+    }
+
+    final newX = position.x + x * _speed;
+    final newY = position.y + y * _speed;
+    position = Vector2(newX, newY);
+
+    angle = angles[direction] ?? angle;
+  }
 }
 
-class MyGame extends FlameGame {
+class MyGame extends FlameGame with HasDraggableComponents {
+  final tank = Tank();
+  final controller = CrossController();
+
   @override
-  Color backgroundColor() => Colors.lightBlue;
+  Future<void> onLoad() async {
+    tank.center = Vector2(size.x / 2, size.y / 2);
+    await add(tank);
+
+    controller.center = Vector2(size.x / 2, size.y - 150);
+    await add(controller);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    tank.move(controller.direction);
+  }
+
+  @override
+  Color backgroundColor() => Colors.grey;
+}
+
+void main() {
+  final game = MyGame();
+  runApp(GameWidget(game: game));
 }
--- lib/controllers_flame.dart
+++ lib/controllers_flame.dart
@@ -1,1 +1,70 @@
 library controllers_flame;
+
+import 'dart:math';
+
+import 'package:flame/cache.dart';
+import 'package:flame/components.dart';
+import 'package:flame/experimental.dart';
+import 'package:flame/flame.dart';
+
+class CrossController extends SpriteComponent with DragCallbacks {
+  static const String _imagesPrefix =
+      'packages/controllers_flame/assets/images/';
+  static const String _defaultSpriteName = 'cross_flat_dark.png';
+  static final Vector2 _defaultDirection = Vector2.zero();
+  static final Vector2 _defaultSize = Vector2.all(150);
+  static const int _degreesRange = 20;
+
+  final String _spriteName;
+  Vector2 _direction = _defaultDirection;
+
+  CrossController({String? spriteName, Vector2? size})
+      : _spriteName = spriteName ?? _defaultSpriteName,
+        super(size: size ?? _defaultSize);
+
+  @override
+  Future<void> onLoad() async {
+    final images = _spriteName != _defaultSpriteName
+        ? Flame.images
+        : Images(prefix: _imagesPrefix);
+    sprite = await Sprite.load(_spriteName, images: images);
+  }
+
+  Vector2 get direction => _direction;
+
+  @override
+  void onDragStart(DragStartEvent event) {
+    _calculate(event.localPosition);
+  }
+
+  @override
+  void onDragUpdate(DragUpdateEvent event) {
+    _calculate(event.localPosition);
+  }
+
+  @override
+  void onDragEnd(DragEndEvent event) {
+    _direction = _defaultDirection;
+  }
+
+  _calculate(Vector2 position) {
+    final offsetX = position.x - width / 2;
+    final offsetY = position.y - height / 2;
+
+    final theta = atan2(offsetY, offsetX);
+    final degrees = (theta / pi * 180) + (theta > 0 ? 0 : 360);
+    if (degrees <= _degreesRange || degrees >= 360 - _degreesRange) {
+      _direction = Vector2(1, 0);
+    } else if (degrees <= 90 + _degreesRange && degrees >= 90 - _degreesRange) {
+      _direction = Vector2(0, 1);
+    } else if (degrees <= 180 + _degreesRange &&
+        degrees >= 180 - _degreesRange) {
+      _direction = Vector2(-1, 0);
+    } else if (degrees <= 270 + _degreesRange &&
+        degrees >= 270 - _degreesRange) {
+      _direction = Vector2(0, -1);
+    } else {
+      _direction = _defaultDirection;
+    }
+  }
+}
